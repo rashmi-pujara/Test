71e2025baf07c4c043036005a9f7913ebcbe90c9
rashmi-pujara <rashmi.pujara1@gmail.com>
2020-09-10 17:44:37 +0530

    commit

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Test
+namespace ConsoleApplication1
 {
-    public class Test
+    class Program
     {
         static void Main(string[] args)
         {
